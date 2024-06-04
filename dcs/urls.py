8dab3e668ccba4d3d62fbbb5f21326dddb239eb8
dimitrigulua <dito.gulua03@gmail.com>
2024-06-04 21:47:48 +0400

    Configure urls, add superuser creation

--- dcs/urls.py
+++ dcs/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt'))
+    path('admin/', admin.site.urls),
+    path('/api/accounts/', include('accounts.urls'))
 ]
 
 # urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
