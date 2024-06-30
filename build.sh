cab0a8ecbdd2925297fe4be73be39121a99d2f0d
Dimitri Gulua <dito.gulua03@gmail.com>
2024-06-30 18:52:24 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-./manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin123@admin.com', 'admin', 'TestPass123')"
+# ./manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin123@admin.com', 'admin', 'TestPass123')"
