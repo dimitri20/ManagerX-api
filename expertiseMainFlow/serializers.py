920887c00590d05a821043d382aa2415e6e37148
dimitri_gulua <dimitri_gulua@singular.uk>
2024-07-14 18:44:14 +0400

    configure mail client, save emails in database and add import to folder function

--- expertiseMainFlow/serializers.py
+++ expertiseMainFlow/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import File, ExpertiseFolder, Tag
+from django_mailbox.models import Message, Mailbox
+from django_mailbox.models import MessageAttachment
 
 
 class FileSerializer(serializers.ModelSerializer):
@@ -33,3 +35,30 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = '__all__'
         read_only_fields = ('uuid', 'created_at', 'updated_at')
+
+
+class EmailMessageAttachmentSerializer(serializers.ModelSerializer):
+    filename = serializers.SerializerMethodField()
+
+    class Meta:
+        model = MessageAttachment
+        fields = '__all__'
+
+    def get_filename(self, obj):
+        return obj.get_filename()
+
+
+class EmailSerializer(serializers.ModelSerializer):
+    attachments = EmailMessageAttachmentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Message
+        exclude = ('body',)
+
+    def get_attachments(self, obj):
+        return EmailMessageAttachmentSerializer.objects.get(message_id=obj.id)
+
+
+class ImportAttachmentsFromMailSerializer(serializers.Serializer):
+    email_id = serializers.IntegerField()
+    copy_to_folder_id = serializers.CharField(max_length=255, allow_blank=False, allow_null=False)
--- expertiseMainFlow/urls.py
+++ expertiseMainFlow/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('expertise/folder/<str:uuid>/details/', views.ExpertiseFolderDetailsView.as_view(), name='folder-details'),
     path('expertise/file/', views.UploadFileView.as_view(), name='upload-file'),
     path('expertise/folder/list/', views.ListExpertiseFolderView.as_view(), name='folder-list'),
-    path('email/test/', views.SendEmailTestView.as_view(), name='email-test')
+    path('email/<str:name>/messages/list/', views.EMailMessagesListView.as_view(), name='email-messages-list'),
+    path('email/messages/import-files/', views.ImportAttachmentsFromMail.as_view(), name='email-import-files'),
+    path('email/<str:name>/fetch/', views.FetchMailWithServer.as_view(), name='fetch-mail')
 ]
